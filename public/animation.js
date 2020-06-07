fd98c475519c34ff49e8fce74a966ee1694808c4
Ricardo Tlatelpa <rtlatlepa4@gmail.com>
2020-06-07 19:10:19 -0400

    gsap implementation

---
+++ public/animation.js
@@ -0,0 +1,21 @@
+const controller = new ScrollMagic.Controller();
+const landingIn = new TimelineMax();
+
+
+
+$(".project").each(function(){
+
+const eachTitle = $(this).find('.project-title');
+const ownContent = $(this).find('.project-info');
+const animateIn = new TimelineMax();
+
+animateIn.from(eachTitle, 0.3, {scaleY: 0, transformOrigin: "bottom"})
+.from(ownContent, 0.2, {scaleY: 0, transformOrigin: "bottom"})
+
+const scene = new ScrollMagic.Scene({
+    triggerElement: this
+})
+.setTween(animateIn).addTo(controller)
+
+})
+
